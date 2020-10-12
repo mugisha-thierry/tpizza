542ce1f139850a518a59d324d00deeac303e3b9d
Mugisha Thierry <thierrymugis@gmail.com>
2020-10-12 15:33:14 +0200

    removed same function

--- java/java.js
+++ java/java.js
@@ -1,17 +1,4 @@
-// function Order(size,crust,toppings=[]) {
-// this.size = size;
-// this.crust = crust;
-// this.toppings = toppings;
-// }
-// Order.prototype.show = function(){
-//     console.log(Order);
-// }
-// var priceSize=parseInt(document.getElementById("size").value);
-// var priceCrust= parseInt(document.getElementById("size").value);
 
-// var thierry = new Order(size= priceSize, crust= priceCrust);
-
-// thierry.show();
 var total;
 
 function show(){
@@ -30,21 +17,11 @@ var sum = topping.reduce(function(a, b){
     return a + b;
 }, 0);
  total= (priceCrust + priceSize + sum)*number;
-// console.log(priceSize);
-// console.log(priceCrust);
-// console.log(topping);
-// console.log(sum);
 document.getElementById('messa').value ="The amount to be paid for the pizza is :"+ " " + total;
 }
 function address(){
     alert("fill the address from where your Pizza will be delivered")
 }
-
-// document.getElementById("myBtn").addEventListener("click", myFunction);
-
-// function myFunction() {
-//   alert ("Hello World!");
-// }
 $(document).ready(function () {
 $('#yes').click(function () {
     $('#addr').show()
